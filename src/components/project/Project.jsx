74b7ff0dd4fffaf752a04a35c0eb619138078016
walidhkhan <walidhkhan@gmail.com>
2021-12-27 13:57:51 -0700

    commit for Project & ProjectList error resolution

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,10 +2,14 @@ import "./project.css"
 
 const Project = () => {
     return (
-        <div>
-            card
+        <div className="p">
+            <div className="p-browser">
+                <div className="p-circle"></div>
+                <div className="p-circle"></div>
+                <div className="p-circle"></div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Project
