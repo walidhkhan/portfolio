d840f9663af2fd869d305600ddda38f6147586ee
walidhkhan <walidhkhan@gmail.com>
2021-12-28 15:47:03 -0700

    added dark mode functionality with toggle Component and context.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useContext } from "react";
+import Toggle from "./components/toggle/Toggle";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/introduction/intro";
 import ProjectList from "./components/projectList/ProjectList";
+import { ThemeContext } from "./context";
 
 const App = () => {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
   return (
-  <div>
-    <Intro/>
-    <About/>
-    <ProjectList/>
-    <Contact/>
+    <div
+      style={{
+        backgroundColor: darkMode ? "black" : "white",
+        color: darkMode && "white",
+      }}>
+      <Toggle />
+      <Intro />
+      <About />
+      <ProjectList />
+      <Contact />
     </div>
   );
 };
--- src/components/introduction/intro.jsx
+++ src/components/introduction/intro.jsx
@@ -1,5 +1,5 @@
 import "./intro.css";
-import Me from "../../img/me.png";
+import Me from "../../img/me2.png";
 
 const Intro = () => {
     return (
