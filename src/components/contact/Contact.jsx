75e335ab16938a7b9cdb6cf6f8ca6577ac19573d
walidhkhan <walidhkhan@gmail.com>
2021-12-29 11:25:05 -0700

    added mobile css responsiveness and resume download link

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./contact.css";
 import Email from "../../img/email.png";
 import LinkedIn from "../../img/linkedin.png";
@@ -6,11 +7,11 @@ import Resume from "../../img/resume.png";
 import { useContext, useRef, useState } from "react";
 import emailjs from 'emailjs-com';
 import { ThemeContext } from "../../context";
-
+import resumeLink from "../../resume/Web-Dev-Resume.pdf";
 
 const Contact = () => {
     const formRef = useRef()
-    const [done, setDone ] = useState(false)
+    const [done, setDone] = useState(false)
     const theme = useContext(ThemeContext)
     const darkMode = theme.state.darkMode;
 
@@ -41,27 +42,33 @@ const Contact = () => {
                             walidhkhan@gmail.com
                         </div>
                         <div className="c-info-item">
-                            <img
-                                src={LinkedIn}
-                                alt=""
-                                className="c-icon"
-                            />
+                            <a href="https://www.linkedin.com/in/walidhkhan/">
+                                <img
+                                    src={LinkedIn}
+                                    alt=""
+                                    className="c-icon"
+                                />
+                            </a>
                             Linkedin
                         </div>
                         <div className="c-info-item">
-                            <img
-                                src={Github}
-                                alt=""
-                                className="c-icon"
-                            />
+                            <a href="https://github.com/walidhkhan">
+                                <img
+                                    src={Github}
+                                    alt=""
+                                    className="c-icon"
+                                />
+                            </a>
                             Github
                         </div>
                         <div className="c-info-item">
-                            <img
-                                src={Resume}
-                                alt=""
-                                className="c-icon"
-                            />
+                            <a href={resumeLink} download>
+                                <img
+                                    src={Resume}
+                                    alt=""
+                                    className="c-icon"
+                                />
+                            </a>
                             Resume
                         </div>
                     </div>
@@ -71,10 +78,10 @@ const Contact = () => {
                         <b>Want to connect?</b> Let's get in touch! I'm always looking for potential opportunities or feedback on my project work 😄
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
-                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
-                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
-                        <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
+                        <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message" />
                         <button>Submit</button>
                         {done && "Your message has been sent. Thank you!"}
                     </form>
