7b1cb1dada5745463d03cb9e072a8b8f6e35aa7f
walidhkhan <walidhkhan@gmail.com>
2022-01-05 05:06:20 -0700

    updated project images to GIFs

--- src/data.js
+++ src/data.js
@@ -3,19 +3,19 @@ export const projects = [
     {
         // Weather Dashboard
         id: 1,
-        img: "https://i.ibb.co/CQVrBYH/weather-dashboard.png",
+        img: "https://i.ibb.co/bv9TJ4w/Weather-Dashboard.gif",
         link: "https://walidhkhan.github.io/weather-dashboard/",
     },
     {
         // Coding Quiz Challenge
         id: 2,
-        img: "https://i.ibb.co/8xqK9Kz/coding-quiz-challenge.png",
+        img: "https://i.ibb.co/VDmkJNd/Coding-Quiz-Challenge.gif",
         link: "https://walidhkhan.github.io/code-quiz/",
     },
     {
         // Professional README.md Generator
         id: 3,
-        img: "https://i.ibb.co/CW4HrJy/readme-gen-pic.png",
+        img: "https://i.ibb.co/Y7fR13x/Professional-Readme-Generator-Walkthrough-Video-You-Tube.gif",
         link: "https://github.com/walidhkhan/professional-readme-generator",
 
     },
