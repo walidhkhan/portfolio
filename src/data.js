068dc9d97bd4a05ef059d9da65b28b8f1f9cdcb8
walidhkhan <walidhkhan@gmail.com>
2022-01-15 21:04:18 -0700

    updated my projects font size for mobile view and switched projects list order

--- src/data.js
+++ src/data.js
@@ -13,20 +13,8 @@ export const projects = [
         imgWidth: "120%",
     },
     {
-        // Coding Quiz Challenge
-        id: 2,
-        img: "https://i.ibb.co/VDmkJNd/Coding-Quiz-Challenge.gif",
-        link: "https://walidhkhan.github.io/code-quiz/",
-        title: "Coding Quiz Challenge",
-        desc: "Quiz featuring coding questions about basic HTML, CSS, and JavaScript.",
-        techs: "HTML, CSS, & JavaScript",
-        gl: "https://github.com/walidhkhan/professional-readme-generator",
-        imgHeight: "60%",
-        imgWidth: "100%",
-    },
-    {
         // Professional README.md Generator
-        id: 3,
+        id: 2,
         img: "https://i.ibb.co/Y7fR13x/Professional-Readme-Generator-Walkthrough-Video-You-Tube.gif",
         link: "https://www.youtube.com/watch?v=_Ew0LdtDh3Y",
         title: "Professional README.md Generator",
@@ -36,4 +24,28 @@ export const projects = [
         imgHeight: "60%",
         imgWidth: "100%",
     },
+    {
+        // Coding Quiz Challenge
+        id: 3,
+        img: "https://i.ibb.co/VDmkJNd/Coding-Quiz-Challenge.gif",
+        link: "https://walidhkhan.github.io/code-quiz/",
+        title: "Coding Quiz Challenge",
+        desc: "Quiz featuring coding questions about basic HTML, CSS, and JavaScript.",
+        techs: "HTML, CSS, & JavaScript",
+        gl: "https://github.com/walidhkhan/professional-readme-generator",
+        imgHeight: "60%",
+        imgWidth: "100%",
+    },
+    // SAMPLE
+    // {
+    //     id: 4,
+    //     img: "",
+    //     link: "",
+    //     title: "",
+    //     desc: "ReadME.md file generator which generates based on user prompt responses.",
+    //     techs: "NodeJS & Inquirer package",
+    //     gl: "https://github.com/walidhkhan/professional-readme-generator",
+    //     imgHeight: "60%",
+    //     imgWidth: "100%",
+    // }
 ];
